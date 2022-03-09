48c1fd1621a17e883d839147baea68294ad01e1c
Muhammad Rizki Darmawan <muhrizdar@gmail.com>
2022-03-09 23:10:25 +0700

    initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import { Article } from "@components/Article";
+
+export default function About() {
+    return (
+        <Article>
+            <h1>About this blog</h1>
+            <p>The best blog. ever.</p>
+        </Article>
+    )
+}
